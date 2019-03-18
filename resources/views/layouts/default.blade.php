b02e930153b4b1ff700c0f94bd04f59fe89464fa
vagrant <vagrant@homestead>
2019-03-18 15:31:52 +0000

    基础页面

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>@yield('title', 'Weibo App') - Laravel 练习项目Weibo App</title>
+	<link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
 </head>
 <body>
 	@yield('content')
