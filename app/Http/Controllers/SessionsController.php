6dc9efd71c7ccd768b9a4acb8d86d582ed540568
vagrant <vagrant@homestead>
2019-03-19 15:46:49 +0000

    访问路由

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,13 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
     	return view('sessions.create');
@@ -22,7 +29,8 @@ class SessionsController extends Controller
     	if(Auth::attempt($credential, $request->has('remember'))) {
     		// 登陆成功后的相关操作
     		session()->flash('success', '欢迎回来!');
-    		return redirect()->route('users.show', [Auth::user()]);	
+            $fallback = route('users.show', Auth::user());
+    		return redirect()->intended($fallback);	
     	} else {
     		// 登陆失败后的相关超赞
     		session()->flash('danger','很抱歉,您的邮箱和密码不匹配');
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,16 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+        $this->middleware('guest', [
+            'only' => ['create']
+        ])
+    }
+
 	public function index()
     {
     	
@@ -19,6 +29,7 @@ class UsersController extends Controller
 
     public function edit(User $user) 
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
@@ -46,6 +57,7 @@ class UsersController extends Controller
 
     public function update(User $user, Request $request)
     {
+        $this->authorize('update', $user);
         $this->validate($request, [
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
