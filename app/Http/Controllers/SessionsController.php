2f2e48a8676582c91d2e54db48a844edd3117242
vagrant <vagrant@homestead>
2019-03-19 14:41:57 +0000

    登陆时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -19,7 +19,7 @@ class SessionsController extends Controller
     		'password' => 'required'
     	]);
 
-    	if(Auth::attempt($credential)) {
+    	if(Auth::attempt($credential, $request->has('remember'))) {
     		// 登陆成功后的相关操作
     		session()->flash('success', '欢迎回来!');
     		return redirect()->route('users.show', [Auth::user()]);	
